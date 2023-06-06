32df3d398746dd32740dc13160bec83ca72858d6
Mateusz Polis <polismateusz@gmail.com>
2023-06-06 15:57:00 +0200

    Calculating biorythm, other improovements

--- models/match.js
+++ models/match.js
@@ -45,6 +45,22 @@ const matchSchema = new mongoose.Schema({
   biorythmEmotional2: {
     type: Number,
   },
+  playerName1: {
+    type: String,
+    default: "",
+  },
+  playerName2: {
+    type: String,
+    default: "",
+  },
+  winnerName: { 
+    type: String,
+    default: "",  
+  },
+  tournamentName: { 
+    type: String,
+    default: "", 
+  },
 });
 
 module.exports = mongoose.model("Match", matchSchema);
--- routes/matches.js
+++ routes/matches.js
@@ -30,6 +30,16 @@ router.get("/new", async (req, res) => {
 router.post("/", async (req, res) => {
   const winner =
     req.body.winner === "player1" ? req.body.player1 : req.body.player2;
+  const biorythms = await calculateBiorythms(
+    req.body.date,
+    req.body.player1,
+    req.body.player2,
+    winner,
+    req.body.tournament
+  );
+
+  console.log(biorythms);
+
   const match = new Match({
     player1: req.body.player1,
     player2: req.body.player2,
@@ -38,8 +48,15 @@ router.post("/", async (req, res) => {
     winner: winner,
     date: new Date(req.body.date),
     tournament: req.body.tournament,
+    biorythmPhysical1: biorythms.physical1.toPrecision(3),
+    biorythmEmotional1: biorythms.emotional1.toPrecision(3),
+    biorythmPhysical2: biorythms.physical2.toPrecision(3),
+    biorythmEmotional2: biorythms.emotional2.toPrecision(3),
+    playerName1: biorythms.playerName1,
+    playerName2: biorythms.playerName2,
+    winnerName: biorythms.winnerName,
+    tournamentName: biorythms.tournamentName,
   });
-  console.log(match);
   try {
     await match.save();
     res.redirect("matches");
@@ -64,4 +81,32 @@ async function renderNewPage(res, match, hasError = false) {
   } catch {
     res.redirect("/matches");
   }
+}
+
+async function calculateBiorythms(date, player1, player2, winner, tournament) {
+  date = new Date(date);
+  let diffTime1, diffDays1, diffTime2, diffDays2;
+
+  player1 = await Player.findById(player1);
+  player2 = await Player.findById(player2);
+  winner = await Player.findById(winner);
+  tournament = await Tournament.findById(tournament);
+
+  birthdate1 = new Date(player1.birthdate);
+  diffTime1 = Math.abs(date.getTime() - birthdate1.getTime());
+  diffDays1 = Math.ceil(diffTime1 / (1000 * 60 * 60 * 24));
+  birthdate2 = new Date(player2.birthdate);
+  diffTime2 = Math.abs(date.getTime() - birthdate2.getTime());
+  diffDays2 = Math.ceil(diffTime2 / (1000 * 60 * 60 * 24));
+
+  return {
+    physical1: Math.sin((2 * Math.PI * diffDays1) / 23),
+    emotional1: Math.sin((2 * Math.PI * diffDays1) / 28),
+    physical2: Math.sin((2 * Math.PI * diffDays2) / 23),
+    emotional2: Math.sin((2 * Math.PI * diffDays2) / 28),
+    playerName1: player1.name,
+    playerName2: player2.name,
+    winnerName: winner.name,
+    tournamentName: tournament.name,
+  };
 }
